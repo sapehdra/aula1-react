a92325641eb4bf4c40f950cca4fd92571b4da0f2
sapehdra <36782079+sapehdra@users.noreply.github.com>
2023-12-18 22:33:53 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="cartao-visitas">
+      <h1>Ada</h1>
+      <p>Estudante de Sistemas de Informação</p>
+      <p>Endereço de Email: sapehdra2@gmail.com</p>
+      <p>Número de Telefone: (555) 99999-9999</p>
+    </div>
+  );
+}
+
+export default App
